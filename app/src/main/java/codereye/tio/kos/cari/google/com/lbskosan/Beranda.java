4f1578fa38c15052759ce6cc504fb3625c7b9e35
tio <dev.tio.saputra@gmail.com>
2018-01-03 18:32:20 +0700

    Keren Aiihhh

--- app/src/main/java/codereye/tio/kos/cari/google/com/lbskosan/Beranda.java
+++ app/src/main/java/codereye/tio/kos/cari/google/com/lbskosan/Beranda.java
@@ -3,11 +3,18 @@ package codereye.tio.kos.cari.google.com.lbskosan;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by tio on 1/3/18.
@@ -16,6 +23,8 @@ import android.view.ViewGroup;
 public class Beranda extends Fragment {
 
     private MainActivity mainActivity;
+    private TabLayout tabLayout;
+    private ViewPager vwPager;
 
     @Override
     public void onAttach(Activity activity) {
@@ -32,20 +41,66 @@ public class Beranda extends Fragment {
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
-        initializeMap();
         super.onCreate(savedInstanceState);
-    }
+//        initializeMap();
 
+
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.beranda, container, false);
+
+        View RootView = inflater.inflate(R.layout.beranda, container, false);
+
+        vwPager = (ViewPager) RootView.findViewById(R.id.viewpager);
+        tabLayout = (TabLayout) RootView.findViewById(R.id.beranda_tabs);
+
+        setupViewPager(vwPager);
+        tabLayout.setupWithViewPager(vwPager);
+        return RootView;
     }
 
-    private void initializeMap(){
-        FragmentTransaction transaction = mainActivity.getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.beranda_container, MapsActivity.newInstance());
-        transaction.commit();
+//    private void initializeMap(){
+//        FragmentTransaction transaction = mainActivity.getSupportFragmentManager().beginTransaction();
+//        transaction.replace(R.id.beranda_container, MapsActivity.newInstance());
+//        transaction.commit();
+//    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(mainActivity.getSupportFragmentManager());
+        adapter.addFragment(new ListKost(), "DAFTAR");
+        adapter.addFragment(new MapsActivity(), "PETA");
+        viewPager.setAdapter(adapter);
+    }
+
+
+    class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager) {
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title) {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mFragmentTitleList.get(position);
+        }
     }
 }
--- app/src/main/java/codereye/tio/kos/cari/google/com/lbskosan/ListKost.java
+++ app/src/main/java/codereye/tio/kos/cari/google/com/lbskosan/ListKost.java
@@ -1,19 +1,29 @@
 package codereye.tio.kos.cari.google.com.lbskosan;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 /**
  * Created by tio on 1/3/18.
  */
 
-public class ListKost extends Fragment {
+public class ListKost extends Fragment implements AdapterView.OnItemClickListener{
 
 
+    ListView listView;
+    String nama_kos[] = {"Kostan dua ", "Wijaya Residen", "Rumah ragil ajah"};
+    String alamat_kos[] = {"Jl Cemaran dua way halim", "Jl Kemang Kemang way duru ah", "Jalan puter lagu dangdut yang"};
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,6 +33,53 @@ public class ListKost extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View RootView = inflater.inflate(R.layout.list_kost, container, false);
+        RootView = fillFoodList(RootView);
+        return RootView;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    private View fillFoodList(View RootView){
+        listView = (ListView) RootView.findViewById(R.id.beranda_list);
+        Customadapter customadapter = new Customadapter();
+        listView.setAdapter(customadapter);
+        listView.setOnItemClickListener(this);
+        return RootView;
+    }
+
+    class Customadapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return nama_kos.length;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup parent) {
+            LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService( Context.LAYOUT_INFLATER_SERVICE );
+            view = inflater.inflate(R.layout.custom_list_beranda, null);
+            ImageView vwGambar = (ImageView) view.findViewById(R.id.list_item_gambar);
+            TextView txtNama = (TextView) view.findViewById(R.id.list_item_nama);
+            TextView txtAlamat = (TextView) view.findViewById(R.id.list_item_alamat);
+
+            vwGambar.setImageResource(R.drawable.rumah_contoh);
+            txtNama.setText(nama_kos[i]);
+            txtAlamat.setText(alamat_kos[i]);
+            return view;
+        }
     }
 }
