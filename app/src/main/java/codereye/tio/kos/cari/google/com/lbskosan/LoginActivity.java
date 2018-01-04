8c91c382f9a80c8146e1634d6e7440d2315c7757
Muhammad Aziz <azizshotokanryu212@gmail.com>
2018-01-04 09:45:54 +0700

    Tambahan stroke button & Ubah Toolbar color

---
+++ app/src/main/java/codereye/tio/kos/cari/google/com/lbskosan/LoginActivity.java
@@ -0,0 +1,13 @@
+package codereye.tio.kos.cari.google.com.lbskosan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
